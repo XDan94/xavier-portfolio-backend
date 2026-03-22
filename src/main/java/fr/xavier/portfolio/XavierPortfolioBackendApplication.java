d832e38ef375aae774e8d210dd48d9ccb143317b
XavierDan94 <dan.xavier94@gmail.com>
2026-03-22 20:33:56 +0100

    init: spring boot 3.5.0 + postgresql

---
+++ src/main/java/fr/xavier/portfolio/XavierPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.xavier.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XavierPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XavierPortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/xavier/portfolio/XavierPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.xavier.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XavierPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
